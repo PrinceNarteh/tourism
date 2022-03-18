f2efd6ce0c6bea36a503116e3d3eb934da336bdd
Prince Narteh <prinarteh@gmail.com>
2022-03-18 07:24:31 +0000

    adds and initialize next-auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { theme } from "styles/theme";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />;
-    </ThemeProvider>
+    <SessionProvider session={session}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />;
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
 
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,5 @@
+import NextAuth from "next-auth/next";
+
+export default NextAuth({
+  providers: [],
+});
