7dffb0d0a33d4e91cf589a2753e2cf1704637e71
Prince Narteh <prinarteh@gmail.com>
2022-03-25 05:05:18 +0000

    add db connection

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGODB_URI;
+const MONGO_URI = process.env.MONGODB_URI || "";
 
 if (!MONGO_URI) {
   throw new Error(
--- pages/api/tourist-sites/index.tsx
+++ pages/api/tourist-sites/index.tsx
@@ -1,14 +1,20 @@
+import dbConnect from "lib/dbConnect";
+import TouristSiteModel from "models/tourist-site.model";
 import { NextApiRequest, NextApiResponse } from "next";
-import TouristSite from "../../../models/tourist-site.model";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
+  await dbConnect();
+  const { method } = req;
+
+  if (method === "POST") {
     res.status(200).json({ message: "Post created" });
-  } else if (req.method === "GET") {
-    const touristSites = await TouristSite.find({});
-    res.status(200).json({ status: "success" });
+  }
+
+  if (method === "GET") {
+    const touristSites = await TouristSiteModel.find({});
+    res.status(200).json({ status: "success", touristSites });
   }
 }
