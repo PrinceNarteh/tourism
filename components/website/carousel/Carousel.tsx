3d56adf093b0d88e07ad8e99a1c781ab79c2d3a5
Prince Narteh <prinarteh@gmail.com>
2022-04-04 07:35:31 +0000

    add random carousel effect

--- components/website/carousel/Carousel.tsx
+++ components/website/carousel/Carousel.tsx
@@ -1,47 +1,127 @@
 import { useAnimation } from "framer-motion";
-import React from "react";
+import React, { useState } from "react";
 
 // SwiperJS
-import { Autoplay, EffectFade, Navigation } from "swiper";
+import {
+  Autoplay,
+  EffectFade,
+  Navigation,
+  EffectCube,
+  EffectFlip,
+  EffectCreative,
+} from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/bundle";
 import { Slider } from "./Slider";
+import { sliders } from "./carouselData";
 
-import BotiFalls from "assets/images/boti-falls.jpg";
-import CapeCoastCastle from "assets/images/cape-coast-castle.jpg";
-import Kakum from "assets/images/kakum.jpg";
-
-const sliders = [
-  {
-    image: Kakum,
-    title: "Kakum National Park",
-    description:
-      "The Kakum National Park is one of the top attraction sites for tourists who intend on seeing wildlife and nature in Ghana",
-  },
-  {
-    image: BotiFalls,
-    title: "Boti Water Falls",
-    description:
-      "Boti falls is a twin waterfall located at Boti in Yilo Krobo District in the Eastern Region of Ghana. These twin falls are referred to as female and male.",
-  },
-  {
-    image: CapeCoastCastle,
-    title: "Cape Coast Castle",
-    description:
-      'Cape Coast Castle is one of about forty "slave castles", or large commercial forts, built on the Gold Coast of West Africa by European traders.',
-  },
-];
+const effects = ["fade", "creative", "flip", "slide"] as const;
 
 export const Carousel = () => {
+  const [effect, setEffect] = useState<
+    "fade" | "creative" | "slide" | "flip" | "cube" | "coverflow" | "cards"
+  >("creative");
   const h3Control = useAnimation();
 
+  const creativeEffect = {
+    prev: {
+      shadow: true,
+      translate: [0, 0, -400],
+    },
+    next: {
+      translate: ["100%", 0, 0],
+    },
+  };
+
+  const handleSetEffect = () => {
+    const randomInt = Math.floor(Math.random() * effects.length);
+    const chosenEffect = effects[randomInt];
+    console.log(chosenEffect);
+    setEffect(chosenEffect);
+  };
+
+  if (effect === "fade") {
+    return (
+      <Swiper
+        modules={[Autoplay, Navigation, EffectFade]}
+        slidesPerView={1}
+        speed={2000}
+        navigation
+        autoplay={{ delay: 5000 }}
+        onSlideChange={() => handleSetEffect()}
+        effect={`${effect}`}
+      >
+        {sliders.map((slider, idx) => (
+          <SwiperSlide key={idx}>
+            <Slider
+              image={slider.image}
+              title={slider.title}
+              description={slider.description}
+              controls={h3Control}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    );
+  } else if (effect === "creative") {
+    return (
+      <Swiper
+        modules={[Autoplay, Navigation, EffectCreative]}
+        slidesPerView={1}
+        speed={2000}
+        navigation
+        creativeEffect={creativeEffect}
+        autoplay={{ delay: 5000 }}
+        onSlideChange={() => handleSetEffect()}
+        effect={`${effect}`}
+      >
+        {sliders.map((slider, idx) => (
+          <SwiperSlide key={idx}>
+            <Slider
+              image={slider.image}
+              title={slider.title}
+              description={slider.description}
+              controls={h3Control}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    );
+  } else if (effect === "flip") {
+    return (
+      <Swiper
+        modules={[Autoplay, Navigation, EffectFlip]}
+        slidesPerView={1}
+        speed={2000}
+        navigation
+        creativeEffect={creativeEffect}
+        autoplay={{ delay: 5000 }}
+        onSlideChange={() => handleSetEffect()}
+        effect={`${effect}`}
+      >
+        {sliders.map((slider, idx) => (
+          <SwiperSlide key={idx}>
+            <Slider
+              image={slider.image}
+              title={slider.title}
+              description={slider.description}
+              controls={h3Control}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    );
+  } else {
+  }
   return (
     <Swiper
-      modules={[Autoplay, Navigation, EffectFade]}
+      modules={[Autoplay, Navigation]}
       slidesPerView={1}
       speed={2000}
       navigation
       autoplay={{ delay: 5000 }}
+      onSlideChange={() => handleSetEffect()}
+      effect={`${effect}`}
     >
       {sliders.map((slider, idx) => (
         <SwiperSlide key={idx}>
---
+++ components/website/carousel/carouselData.ts
@@ -0,0 +1,24 @@
+import BotiFalls from "assets/images/boti-falls.jpg";
+import CapeCoastCastle from "assets/images/cape-coast-castle.jpg";
+import Kakum from "assets/images/kakum.jpg";
+
+export const sliders = [
+  {
+    image: Kakum,
+    title: "Kakum National Park",
+    description:
+      "The Kakum National Park is one of the top attraction sites for tourists who intend on seeing wildlife and nature in Ghana",
+  },
+  {
+    image: BotiFalls,
+    title: "Boti Water Falls",
+    description:
+      "Boti falls is a twin waterfall located at Boti in Yilo Krobo District in the Eastern Region of Ghana. These twin falls are referred to as female and male.",
+  },
+  {
+    image: CapeCoastCastle,
+    title: "Cape Coast Castle",
+    description:
+      'Cape Coast Castle is one of about forty "slave castles", or large commercial forts, built on the Gold Coast of West Africa by European traders.',
+  },
+];
